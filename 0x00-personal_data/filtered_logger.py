f0e64975bf75eab6749f4d77b4d002e59dbfaf16
Michael Iyke <ymichaelc@gmail.com>
2024-08-01 22:12:03 +0100

    Task 0:  redact data on a log line

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,2 +1,13 @@
 #!/usr/bin/env python3
 """Alx Backend User Data module"""
+from typing import List
+import re
+
+def filter_datum(
+    fields: List[str], redaction: str, message: str, separator: str) -> str:
+    """Obfuscates data on a log line by redaction"""
+    # return re.sub(r'({})'.format(separator.join(fields)), redaction, message)
+    for field in fields:
+        message = re.sub(r'{}=(.*?){}'.format(field, separator),
+                         '{}={}{}'.format(field, redaction, separator), message)
+    return message
