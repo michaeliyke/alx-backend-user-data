0bdd14adb5bb57bfa9fd68e47a34f9235505f9ec
Michael Iyke <ymichaelc@gmail.com>
2024-08-02 18:55:57 +0100

    tasks 0-2

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,12 +2,52 @@
 """Alx Backend User Data module"""
 from typing import List
 import re
+import logging
+import logging
+import csv
+
 
 def filter_datum(
-    fields: List[str], redaction: str, message: str, separator: str) -> str:
+        fields: List[str], redaction: str, message: str, separator: str) -> str:
     """Obfuscates data on a log line by redaction"""
     # return re.sub(r'({})'.format(separator.join(fields)), redaction, message)
     for field in fields:
         message = re.sub(r'{}=(.*?){}'.format(field, separator),
                          '{}={}{}'.format(field, redaction, separator), message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """The constructor of the RedactingFormatter class"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Filters values in incoming log records"""
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
+
+
+PII_FIELDS = ('email', 'phone', 'ssn', 'password', 'ip')
+
+
+def get_logger() -> logging.Logger:
+    """Returns a logging.Logger object"""
+    logger = logging.getLogger('user_data')
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    formatter = RedactingFormatter(PII_FIELDS)
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+
+    logger.addHandler(stream_handler)
+
+    return logger
