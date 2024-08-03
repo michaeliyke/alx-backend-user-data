48baa6d4ea9e0aa44e87717ccea6c13b1c4ded15
Michael Iyke <ymichaelc@gmail.com>
2024-08-03 23:19:38 +0100

    correct

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,13 +1,11 @@
-import bcrypt
-
 #!/usr/bin/env python3
 """Alx Backend User Data module"""
+import bcrypt
 
 
 def hash_password(password: str) -> bytes:
     """Hashes a password and returns a salted hash"""
     # Generate a salt
     salt = bcrypt.gensalt()
-
     # Hash the password with the generated salt
     return bcrypt.hashpw(password.encode(), salt)
