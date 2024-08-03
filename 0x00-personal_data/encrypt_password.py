5edc712896b485d71b525ca3178343b980348b61
Michael Iyke <ymichaelc@gmail.com>
2024-08-03 23:18:54 +0100

    Task 5: Hash password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,2 +1,12 @@
+import bcrypt
+
 #!/usr/bin/env python3
 """Alx Backend User Data module"""
+
+
+def hash_password(password: str) -> bytes:
+    # Generate a salt
+    salt = bcrypt.gensalt()
+
+    # Hash the password with the generated salt
+    return bcrypt.hashpw(password.encode(), salt)
