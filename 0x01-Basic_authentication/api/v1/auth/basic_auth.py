68bd35c1aec2801e4ab5cd8788d060705692aa89
Michael Iyke <ymichaelc@gmail.com>
2024-08-06 11:31:28 +0100

    Task 08: Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,10 +3,24 @@
 from flask import request
 from typing import List, TypeVar
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
     """Basic Auth class implements basic authentication."""
+
+    def decode_base64_authorization_header(self, base64_authorization_header:
+                                           str) -> str:
+        """Decodes a base64 string to get the encoded bytes."""
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            byte_obj = base64.b64decode(base64_authorization_header)
+            return byte_obj.decode('utf-8')
+        except Exception as err:
+            return None
 
     def extract_base64_authorization_header(self,
                                             authorization_header: str) -> str:
