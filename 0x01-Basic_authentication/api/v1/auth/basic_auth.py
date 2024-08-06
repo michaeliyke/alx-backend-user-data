9c56cd9327be1919bdcd8bd7843ca9c3bf736bc9
Michael Iyke <ymichaelc@gmail.com>
2024-08-06 12:11:03 +0100

    Task 10: Basic - User object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,10 +4,28 @@ from flask import request
 from typing import List, TypeVar, Tuple
 from api.v1.auth.auth import Auth
 import base64
+from models.user import User
+
+user = TypeVar('User')
 
 
 class BasicAuth(Auth):
     """Basic Auth class implements basic authentication."""
+
+    def user_object_from_credentials(self, user_email: str, user_pwd:
+                                     str) -> user:
+        """Returns the User instance based on email and password."""
+        if not user_email or not isinstance(user_email, str):
+            return None
+        if not user_pwd or not isinstance(user_pwd, str):
+            return None
+        user = User.search({'email': user_email})
+        if not user:
+            return None
+        user = user[0]
+        if not user.is_valid_password(user_pwd):
+            return None
+        return user
 
     def extract_user_credentials(self, decoded_base64_authorization_header:
                                  str) -> Tuple[str, str]:
