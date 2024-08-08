3b7ae9266cf049c51c04517e9b3bb4fbed0ce251
Michael Iyke <ymichaelc@gmail.com>
2024-08-08 13:20:39 +0100

    Task 02: session id dict store

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -8,7 +8,19 @@ from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
 from api.v1.auth.basic_auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """Session auth wrapper around the Auth class"""
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates session id from user id"""
+        if user_id is None:
+            None
+        if not isinstance(user_id, str):
+            None
+        session_id = uuid4()
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
