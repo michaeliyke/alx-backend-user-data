de661611ccfe3b87461c9bd7d3461d95bb7f7e85
Michael Iyke <ymichaelc@gmail.com>
2024-08-08 14:51:28 +0100

    Task 03: getting cookie

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -15,6 +15,14 @@ class SessionAuth(Auth):
     """Session auth wrapper around the Auth class"""
     user_id_by_session_id = {}
 
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """Returns user id based on session id"""
+        if session_id is None:
+            return None
+        if not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id)
+
     def create_session(self, user_id: str = None) -> str:
         """Creates session id from user id"""
         if user_id is None:
