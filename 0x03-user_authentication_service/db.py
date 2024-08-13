8b569f5cd79bc101ebb97142f6a665ab6bad44ca
Michael Iyke <ymichaelc@gmail.com>
2024-08-13 16:56:14 +0100

    Task 01: Create a user

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+Module for the DB management class
+"""
+from user import Base
+from sqlalchemy.orm.session import Session
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from user import User
+
+
+class DB:
+    """
+    DB class to manage the database connection
+    """
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """
+        Add a new user to the database
+        """
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
+        return user
+
+    def __init__(self) -> None:
+        """
+        Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """
+        Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine, expire_on_commit=False)
+            self.__session = DBSession()
+        return self.__session
