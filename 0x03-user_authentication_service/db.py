7260fa204ac313c16a0b8a1f6ca537064aab12bc
Michael Iyke <ymichaelc@gmail.com>
2024-08-13 19:29:22 +0100

    Task 03:  updating

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -17,6 +17,21 @@ class DB:
     DB class to manage the database connection
     """
 
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """
+        Update a user based on the given user_id and keyword arguments
+        """
+        user = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            # skip the id and hashed_password attributes
+            if key in ["id"]:
+                continue
+            if hasattr(user, key):
+                setattr(user, key, value)
+            else:
+                raise ValueError(f"Invalid attribute: {key}")
+        self._session.commit()
+
     def find_user_by(self, **kwargs) -> User:
         """
         Find a user in the database based on the given keyword arguments
