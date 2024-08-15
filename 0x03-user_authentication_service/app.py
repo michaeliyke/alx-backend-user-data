278ba0b88729feac079842e974b2f2f831935e20
Michael Iyke <ymichaelc@gmail.com>
2024-08-15 19:29:25 +0100

    Task 07: POST user endpoint

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,20 +1,35 @@
 #!/usr/bin/env python3
 """Module for a simple flask app"""
-from flask import Flask, jsonify, make_response
+from flask import Flask, jsonify, make_response, request
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", methods=["GET"], strict_slashes=False)
 def homepage():
-    """The homepage function"""
+    """The homepage endpoint function"""
     return make_response(jsonify({"message": "Bienvenue"}), 200)
 
 
 @app.route("/status", methods=["GET"], strict_slashes=False)
 def status():
-    """The status function"""
+    """/status endpoint function"""
     return make_response(jsonify({"status": "Ok"}), 200)
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users():
+    """/users endpoint function"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    try:
+        AUTH.register_user(email, password)
+        return make_response(jsonify({"email": email, "message": "user created"}), 200)
+    except ValueError:
+        return make_response(jsonify({"message": "email already registered"}), 400)
 
 
 if __name__ == "__main__":
