8ae850a10bdd491f16c5134e3eb5ad1c731a83a1
Michael Iyke <ymichaelc@gmail.com>
2024-08-16 16:34:21 +0100

    task 08 correction

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -21,6 +21,16 @@ class Auth:
     """Auth class to interact with the authentication database.
     """
 
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+        Validates the login credentials for the given email and password.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+        return bcrypt.checkpw(password.encode(), user.hashed_password)
+
     def register_user(self, email: str, password: str) -> User:
         """
         Registers a new user with the given email and password.
